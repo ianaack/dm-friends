071ad04cf019ef69581d91ecb3a8bfd7dde549f2
Michael Hodges <Michael.Hodges1985@yahoo.com>
2023-04-06 22:05:50 -0500

    added conversation model to help organize messages within the scope of a conversation instance.

---
+++ server/models/Conversation.js
@@ -0,0 +1,31 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/connection.js");
+
+class Conversation extends Model {}
+
+Conversation.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        messages: {
+            type: DataTypes.JSON,  //This will be a JSON object that stores an array with the message id's.
+            references: {
+				model: "message",
+				key: "id",
+			},
+        }
+    },
+    {
+		sequelize,
+		timestamps: true,
+		freezeTableName: true,
+		underscored: true,
+		modelName: "conversation",
+	}
+);
+
+module.exports = Conversation;

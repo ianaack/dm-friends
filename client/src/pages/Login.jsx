12a6fd621bc643db0f214887dd94e52edbc68e0b
Brian Crisp <bcrisp084@yahoo.com>
2023-04-10 19:56:47 -0400

    Added a header to the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,25 +2,30 @@ import React from "react";
 
 export default function Login() {
   return (
-    <div className="login-box">
-      <h2>Login</h2>
-      <form>
-        <div className="user-box">
-          <input type="text" name="username" required />
-          <label>Username</label>
-        </div>
-        <div className="user-box">
-          <input type="password" name="password" required />
-          <label>Password</label>
-          <span className="signup-span">
-            Not a member yet?
-            <a className="signup-link" href="/signup">
-              Sign up
-            </a>
-          </span>
-        </div>
-        <button href="/">Submit</button>
-      </form>
-    </div>
+    <>
+      <div>
+        <h1 className="login-title">DM Friends</h1>
+      </div>
+      <div className="login-box">
+        <h2>Login</h2>
+        <form>
+          <div className="user-box">
+            <input type="text" name="username" required />
+            <label>Username</label>
+          </div>
+          <div className="user-box">
+            <input type="password" name="password" required />
+            <label>Password</label>
+            <span className="signup-span">
+              Not a member yet?
+              <a className="signup-link" href="/signup">
+                Sign up
+              </a>
+            </span>
+          </div>
+          <button href="/">Submit</button>
+        </form>
+      </div>
+    </>
   );
 }

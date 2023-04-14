2efd305efaba445913a0c1a7cbc64db570f164d8
ianaack <ianaack@gmail.com>
2023-04-13 20:56:39 -0400

    Feature: built navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import NavBar from "./pages/components/Navbar";
 
 function App() {
-  return <div className="App">
-    <h1>DM Friends</h1>
-    
-  </div>;
+	return (
+		<div className="App">
+			<NavBar />
+			<h1>DM Friends</h1>
+		</div>
+	);
 }
 
 export default App;

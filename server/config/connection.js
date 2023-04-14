7a854f5d8fda393ca008a8616eb4f264040a10e6
ianaack <ianaack@gmail.com>
2023-04-13 20:58:19 -0400

    Feature: connect to Planetscale

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,14 @@
-require("dotenv").config({debug: true});
+require("dotenv").config({ debug: true });
 const Sequelize = require("sequelize");
 const DB_NAME = process.env.DB_NAME;
 const DB_USER = process.env.DB_USER;
 const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_URL = process.env.DATABASE_URL;
 
 let sequelize;
 
-if (process.env.JAWSDB_URL) {
-	sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (DB_URL) {
+	sequelize = new Sequelize(DB_URL);
 } else {
 	sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
 		host: "localhost",
